4424da37cea0be185b572ccef86233e581bd5754
Sumit Jangir <jangirsumit1011@gmail.com>
2024-11-17 17:42:46 +0530

    Add cors

--- CURD Project/Backend/index.js
+++ CURD Project/Backend/index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 require('dotenv').config();
 
 const userRoute = require('./Route/UserRoute')
 
 const app = express();
+
+app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
