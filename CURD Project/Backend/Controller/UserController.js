bbcab49f435918350d55ce50d95c1c7cad3180d6
Sumit Jangir <jangirsumit1011@gmail.com>
2024-11-21 23:55:11 +0530

    Add validation

--- CURD Project/Backend/Controller/UserController.js
+++ CURD Project/Backend/Controller/UserController.js
@@ -1,46 +1,45 @@
-const userModel = require('../Model/UserModel')
+const userModel = require("../Model/UserModel");
 
-exports.findUser = async (req,res)=>{
-    const myData = await userModel.find();
+exports.findUser = async (req, res) => {
+  const myData = await userModel.find();
 
-    if(!myData)
-        res.status(404).json({error: 'Error'})
+  if (!myData) res.status(404).json({ error: "Error" });
+  else res.status(200).json(myData);
+};
 
-    else
-        res.status(200).json(myData);
-}
+exports.deleteUser = async (req, res) => {
+  const { id } = req.params;
+  const myData = await userModel.findByIdAndDelete(id);
 
-exports.deleteUser = async (req,res)=>{
-    const {id} = req.params
-    const myData = await userModel.findByIdAndDelete(id);
+  if (!myData) res.status(404).json({ error: "Error" });
+  else res.status(200).json(myData);
+};
 
-    if(!myData)
-        res.status(404).json({error: 'Error'})
+exports.addUser = async (req, res) => {
+  const { email } = req.body;
 
-    else
-        res.status(200).json(myData);
-}
+  let user = await userModel.findOne({ email });
+  if (user) {
+    return res.status(400).json({message: "Email already exists"})
+  }
 
-exports.addUser = async (req,res)=>{
-    
-    const user = new userModel(req.body)
+  user = new userModel(req.body);
 
-    await user.save();
-    // console.log(user)
-    res.status(200).json(user);
-}
+  await user.save();
+  // console.log(user)
+  res.status(200).json(user);
+};
 
-exports.updateUser = async (req,res)=>{
-    
-    const {id} = req.body;
-    const data = req.body;
+exports.updateUser = async (req, res) => {
+  const { id } = req.body;
+  const data = req.body;
 
-    const user = await userModel.findByIdAndUpdate(id,data)
+  const user = await userModel.findByIdAndUpdate(id, data);
 
-    console.log("<<<<user>>>>",user);
-    if(!user)
-        res.status(404).json({error:'err'});
-    else
-        res.status(200).json(user);
-        
-}
+  console.log("<<<<user>>>>", user);
+  if (!user) {
+    return;
+    res.status(404).json({ error: "err" });
+  }
+  res.status(200).json(user);
+};
